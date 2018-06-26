001fe3f3f85947cc5057811e5b8164b077b1dc19
Kyron Stubbs <B@Kyrons-MacBook-Pro.local>
2018-06-26 10:24:18 -0300

    new spark context; reading in raw data

--- src/com/strategylabs/spark/PurchaseByCustomer.scala
+++ src/com/strategylabs/spark/PurchaseByCustomer.scala
@@ -2,6 +2,7 @@ package com.strategylabs.spark
       
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
 
 class PurchaseByCustomer {
   
@@ -9,6 +10,10 @@ class PurchaseByCustomer {
     
     val logger = Logger.getLogger(this.getClass())
     logger.setLevel(Level.ERROR)
+    
+    val spark = new SparkContext("local[*]", "PurchaseByCustomer")
+    
+    val data = spark.textFile("../spark-scala/customer-orders.csv")
     
     
     
