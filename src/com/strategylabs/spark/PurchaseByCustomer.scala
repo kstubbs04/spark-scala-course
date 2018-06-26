169e01382d9d15bbaecc56cc8a5cd6aafd61df46
Kyron Stubbs <B@Kyrons-MacBook-Pro.local>
2018-06-26 10:22:24 -0300

    added logger

--- src/com/strategylabs/spark/PurchaseByCustomer.scala
+++ src/com/strategylabs/spark/PurchaseByCustomer.scala
@@ -1,5 +1,16 @@
 package com.strategylabs.spark
+      
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 
 class PurchaseByCustomer {
   
+  def main(arguments: Array[String]){
+    
+    val logger = Logger.getLogger(this.getClass())
+    logger.setLevel(Level.ERROR)
+    
+    
+    
+  }
 }
