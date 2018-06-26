e04e3124b357cdd9395219cfe6700bd1b37256f1
Kyron Stubbs <B@Kyrons-MacBook-Pro.local>
2018-06-26 10:30:57 -0300

    new case class and method for processing data

--- src/com/strategylabs/spark/PurchaseByCustomer.scala
+++ src/com/strategylabs/spark/PurchaseByCustomer.scala
@@ -4,7 +4,18 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 import org.apache.spark.SparkContext
 
+case class RawEntry(customerId: Long, amountSpent: Float)
+
 class PurchaseByCustomer {
+  
+  val CustomerIdIndex = 0
+  val AmountSpentIndex = 2  
+  
+  def parseString(line: String) = {
+    val row = line.split(",")
+    RawEntry(row(CustomerIdIndex).toLong, row(AmountSpentIndex).toFloat)
+  }
+  
   
   def main(arguments: Array[String]){
     
@@ -15,6 +26,7 @@ class PurchaseByCustomer {
     
     val data = spark.textFile("../spark-scala/customer-orders.csv")
     
+    val kvData = data.map(parseString)
     
     
   }
